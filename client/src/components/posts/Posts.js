b3ca4ebff45e9769e1835c46fe3e1af31d598b2a
Tom Plucinski <tapluci1@gmail.com>
2020-09-07 15:02:45 -0500

    initial commit

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getPosts } from '../../actions/post';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return loading ? (
+    <div>loading</div>
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the community
+      </p>
+      <PostForm />
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapsStateToProps = state => ({
+  post: state.post,
+});
+
+export default connect(mapsStateToProps, { getPosts })(Posts);
